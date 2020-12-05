f266d7904d0ecd6e48770462c4326b659da61b92
Mark Heath <mark.heath@gmail.com>
2020-12-05 11:52:13 +0000

    Add project files.

---
+++ AdventOfCode2020/Day5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2020
+{
+    interface ISolver
+    {
+        public (string,string) Solve(string[] input);
+    }
+
+    class Day5 : ISolver
+    {
+        public (string, string) Solve(string[] input)
+        {
+            var seats = ParseSeats(input);
+            var partA = seats.Max().ToString(); // 965
+            var partB = Enumerable.Range(0, 128 * 8).Single(s => !seats.Contains(s) && seats.Contains(s - 1) && seats.Contains(s + 1)).ToString(); // 524
+            return (partA, partB);
+        }
+
+        private HashSet<int> ParseSeats(string[] input) => 
+            input.Select(x => Convert.ToInt32(x.Replace('F', '0').Replace('B', '1').Replace('R', '1').Replace('L', '0'), 2)).ToHashSet();
+    }
+}
---
+++ AdventOfCode2020/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var day5 = new Day5();
+            var input = File.ReadAllLines("../../../day5.txt");
+            var (a,b) = day5.Solve(input);
+            Console.WriteLine($"ResultA: {a}");
+            Console.WriteLine($"ResultB: {b}");
+
+        }
+
+    }
+}
