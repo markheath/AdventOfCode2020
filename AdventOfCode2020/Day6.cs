991c262173cf6d3c3f0724d8131c125a13d3f961
Mark Heath <mark.heath@gmail.com>
2020-12-06 12:39:32 +0000

    refactoring day 6

--- AdventOfCode2020/Day6.cs
+++ AdventOfCode2020/Day6.cs
@@ -1,6 +1,4 @@
 ﻿using MoreLinq;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode2020
@@ -11,29 +9,8 @@ namespace AdventOfCode2020
 
         public (string, string) Solve(string[] input)
         {
-
-            var anyCount = 0;
-            var allCount = 0;
-            foreach (var batch in input.Split(""))
-            {
-                var anyAnswer = new HashSet<char>();
-                foreach (var line in batch)
-                {
-                    foreach (var c in line)
-                    {
-                        anyAnswer.Add(c);
-                    }
-                }
-                foreach(var c in batch.First())
-                {
-                    if(batch.All(x => x.Contains(c)))
-                    {
-                        allCount++;
-                    }
-                }
-
-                anyCount += anyAnswer.Count;
-            }
+            var anyCount = input.Split("").Sum(batch => batch.SelectMany(b => b).Distinct().Count());
+            var allCount = input.Split("").Sum(batch => batch.First().Count(c => batch.All(x => x.Contains(c))));
             return (anyCount.ToString(), allCount.ToString());
         }
     }
