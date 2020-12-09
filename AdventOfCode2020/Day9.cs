7ce89fe5acf20154035effe787ea3f3384b90dc8
Mark Heath <mark.heath@gmail.com>
2020-12-09 09:16:25 +0000

    day 9 part 2

--- AdventOfCode2020/Day9.cs
+++ AdventOfCode2020/Day9.cs
@@ -7,13 +7,14 @@ namespace AdventOfCode2020
 {
     public class Day9 : ISolver
     {
-        public (string, string) ExpectedResult => ("1124361034", "");
+        public (string, string) ExpectedResult => ("1124361034", "129444555");
 
         public (string, string) Solve(string[] input)
         {
             var numbers = input.Select(i => long.Parse(i)).ToList();
             var part1 = Solve(numbers, 25);
-            return (part1.ToString(), "");
+            var part2 = Solve2(numbers, part1);
+            return (part1.ToString(), part2.ToString());
         }
 
         public static long Solve(IList<long> numbers, int preamble)
@@ -22,6 +23,25 @@ namespace AdventOfCode2020
             {
                 var found = numbers.Skip(n - preamble).Take(preamble).Subsets(2).Select(s => s[0] + s[1]).Any(sum => sum == numbers[n]);
                 if (!found) return numbers[n];
+            }
+            throw new InvalidOperationException("No solution found");
+        }
+
+        public static long Solve2(IList<long> numbers, long target)
+        {
+            for (var n = 0; n < numbers.Count; n++)
+            {
+                var total = 0L;
+                for (var k = n; k < numbers.Count && total < target; k++)
+                {
+                    total += numbers[k];
+                    if (total == target && n != k)
+                    {
+                        var lowest = numbers.Skip(n).Take(k - n).Min();
+                        var highest = numbers.Skip(n).Take(k - n).Max();
+                        return lowest + highest;
+                    }
+                }
             }
             throw new InvalidOperationException("No solution found");
         }
--- UnitTests/Day9Tests.cs
+++ UnitTests/Day9Tests.cs
@@ -7,7 +7,7 @@ namespace UnitTests
     public class Day9Tests
     {
         [Test]
-        public void SolveWithTestInput()
+        public void SolvePart1WithTestInput()
         {
             var testInput = @"35
 20
@@ -31,6 +31,33 @@ namespace UnitTests
 576".Split("\r\n");
             var answer = Day9.Solve(testInput.Select(long.Parse).ToList(), 5);
             Assert.AreEqual(127, answer);
+        }
+
+        [Test]
+        public void SolvePart2WithTestInput()
+        {
+            var testInput = @"35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576".Split("\r\n");
+            var answer = Day9.Solve2(testInput.Select(long.Parse).ToList(), 127);
+            Assert.AreEqual(62, answer);
         }
 
     }
