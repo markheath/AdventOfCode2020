4b9846fb24e7de5f31c69ec270f2a53f9c58806b
Mark Heath <mark.heath@gmail.com>
2020-12-14 07:39:21 +0000

    day 14 part 1

--- AdventOfCode2020/Day14.cs
+++ AdventOfCode2020/Day14.cs
@@ -1,14 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode2020
 {
     public class Day14 : ISolver
     {
-        public (string, string) ExpectedResult => ("", "");
+        public (string, string) ExpectedResult => ("10452688630537", "");
 
         public (string, string) Solve(string[] input)
         {
-            throw new NotImplementedException();
+            var memory = new Dictionary<long, long>();
+            long andMask = 0;
+            long orMask = 0;
+            foreach(var instruction in input)
+            {
+                if (instruction.StartsWith("mask = "))
+                {
+                    (andMask, orMask) = ParseMask(instruction.Split(' ').Last());
+                }
+                else
+                {
+                    var parts = Regex.Split(instruction, "\\D+");
+                    var location = long.Parse(parts[1]);
+                    var value = long.Parse(parts[2]);
+                    memory[location] = (value & andMask) | orMask;
+                }
+            }
+            var part1 = memory.Sum(m => m.Value);
+            return (part1.ToString(), "");
         }
+
+        public static (long,long) ParseMask(string mask)
+        {
+            long andWith = 0;
+            long orWith = 0; // set all the 1s in here           
+            var bit = 0;
+            foreach(var c in mask.Reverse())
+            {
+                if (c == 'X')
+                {
+                    andWith |= 1L << bit;
+                }
+                else if (c == '1')
+                {
+                    orWith |= 1L << bit;
+                }
+                bit++;
+            }
+            return (andWith, orWith);
+        }
+
     }
 }
--- UnitTests/Day14Tests.cs
+++ UnitTests/Day14Tests.cs
@@ -8,9 +8,13 @@ namespace UnitTests
         [Test]
         public void SolveWithTestInput()
         {
-            var testInput = @"".Split("\r\n");
+            var testInput = @"mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+mem[8] = 11
+mem[7] = 101
+mem[8] = 0".Split("\r\n");
             var answer = new Day14().Solve(testInput);
-            Assert.AreEqual(("a", "b"), answer);
+            Assert.AreEqual(("165", ""), answer);
         }
+
     }
 }
