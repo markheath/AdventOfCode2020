bc91c65fbd91d3f5a938b9390eaa76aeba51828c
Mark Heath <mark.heath@gmail.com>
2020-12-15 07:54:29 +0000

    day 15 part 1

--- AdventOfCode2020/Day15.cs
+++ AdventOfCode2020/Day15.cs
@@ -1,14 +1,50 @@
-﻿using System;
+﻿using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode2020
 {
     public class Day15 : ISolver
     {
-        public (string, string) ExpectedResult => ("", "");
+        public (string, string) ExpectedResult => ("763", "");
 
         public (string, string) Solve(string[] input)
         {
-            throw new NotImplementedException();
+            var part1 = SolvePart1(input[0].Split(',')
+                            .Select(int.Parse).ToList());
+            return (part1.ToString(), "");
+        }
+
+        public static int SolvePart1(IList<int> starting)
+        {
+            var memory = new Dictionary<int, int>();             
+
+            var last = 0;
+
+
+            for (var turn = 1; turn <= 2020; turn++)
+            {
+                var newNumber = 0;
+                if (turn <= starting.Count)
+                {
+                    newNumber = starting[turn - 1];
+                }
+                else
+                {
+                    if (memory.ContainsKey(last))
+                    {
+                        newNumber = turn - 1 - memory[last];
+                    }
+                }
+
+                if (turn > 1)
+                {
+                    memory[last] = turn - 1;
+                }
+                last = newNumber;
+            }
+            return last;
         }
     }
 }
--- UnitTests/Day15Tests.cs
+++ UnitTests/Day15Tests.cs
@@ -1,5 +1,6 @@
 ﻿using AdventOfCode2020;
 using NUnit.Framework;
+using System.Linq;
 
 namespace UnitTests
 {
@@ -8,9 +9,23 @@ namespace UnitTests
         [Test]
         public void SolveWithTestInput()
         {
-            var testInput = @"".Split("\r\n");
+            var testInput = @"0,3,6".Split("\r\n");
             var answer = new Day15().Solve(testInput);
-            Assert.AreEqual(("a", "b"), answer);
+            Assert.AreEqual(("436", ""), answer);
+        }
+
+        [TestCase("0,3,6", 436)]
+        [TestCase("2,1,3", 10)]
+        [TestCase("1,3,2", 1)]
+        [TestCase("2,1,3", 10)]
+        [TestCase("1,2,3", 27)]
+        [TestCase("2,3,1", 78)]
+        [TestCase("3,2,1", 438)]
+        [TestCase("3,1,2", 1836)]
+        public void SolvePart1(string input, int expected)
+        {
+            var answer = Day15.SolvePart1(input.Split(",").Select(int.Parse).ToList());
+            Assert.AreEqual(expected, answer);
         }
     }
 }
