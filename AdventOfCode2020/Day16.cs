16883d6268a2b2489872556123f40bdd775ff474
Mark Heath <mark.heath@gmail.com>
2020-12-16 07:03:54 +0000

    day 16 part 1

--- AdventOfCode2020/Day16.cs
+++ AdventOfCode2020/Day16.cs
@@ -1,14 +1,64 @@
-﻿using System;
+﻿using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode2020
 {
     public class Day16 : ISolver
     {
-        public (string, string) ExpectedResult => ("", "");
+        public (string, string) ExpectedResult => ("23044", "");
 
         public (string, string) Solve(string[] input)
         {
-            throw new NotImplementedException();
+            var parts = input.Split("").ToList();
+            var rules = parts[0].Select(r => new Rule(r)).ToList();
+            var myTicket = new Ticket(parts[1].Last());
+            var nearbyTickets = parts[2].Skip(1).Select(t => new Ticket(t)).ToList();
+            var scanningError = nearbyTickets.Sum(n => n.ScanningError(rules));
+
+            return (scanningError.ToString(), "");
         }
+
+        public class Ticket
+        {
+            private readonly string ticket;
+            public override string ToString() => ticket;
+            private int[] values;
+            public Ticket(string ticket)
+            {
+                values = ticket.Split(',').Select(int.Parse).ToArray();
+                this.ticket = ticket;
+            }
+            public int ScanningError(IEnumerable<Rule> rules)
+            {
+                return values.Where(v => rules.All(r => !r.IsValid(v))).Sum();
+            }
+        }
+
+        public class Rule
+        {
+            private readonly string rule;
+            public override string ToString() => rule;
+            private (int, int) range1;
+            private (int, int) range2;
+            public Rule(string rule)
+            {
+                var idx = rule.IndexOf(':');
+                Name = rule.Substring(0, idx);
+                var parts = rule.Substring(idx+2).Split(" or ");
+                range1 = (int.Parse(parts[0].Split('-')[0]), int.Parse(parts[0].Split('-')[1]));
+                range2 = (int.Parse(parts[1].Split('-')[0]), int.Parse(parts[1].Split('-')[1]));
+                this.rule = rule;
+            }
+            public string Name { get; }
+            public bool IsValid(int n)
+            {
+                return (n >= range1.Item1 && n <= range1.Item2) ||
+                    (n >= range2.Item1 && n <= range2.Item2);
+            }
+
+        }
+
     }
 }
--- UnitTests/Day16Tests.cs
+++ UnitTests/Day16Tests.cs
@@ -8,9 +8,20 @@ namespace UnitTests
         [Test]
         public void SolveWithTestInput()
         {
-            var testInput = @"".Split("\r\n");
+            var testInput = @"class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12".Split("\r\n");
             var answer = new Day16().Solve(testInput);
-            Assert.AreEqual(("a", "b"), answer);
+            Assert.AreEqual(("71", ""), answer);
         }
     }
 }
