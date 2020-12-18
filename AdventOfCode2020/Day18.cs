56c3b407cf5a3ee0d8bacba72722b9cae06a08c2
Mark Heath <mark.heath@gmail.com>
2020-12-18 07:34:08 +0000

    day 18 part 1

--- AdventOfCode2020/Day18.cs
+++ AdventOfCode2020/Day18.cs
@@ -1,14 +1,69 @@
 ﻿using System;
+using System.Linq;
 
 namespace AdventOfCode2020
 {
     public class Day18 : ISolver
     {
-        public (string, string) ExpectedResult => ("", "");
+        public (string, string) ExpectedResult => ("67800526776934", "");
 
         public (string, string) Solve(string[] input)
         {
-            throw new NotImplementedException();
+            var part1 = input.Sum(SolveEquation);
+            return (part1.ToString(), "");
+        }
+
+        public static long SolveEquation(string equation)
+        {
+            long currentTotal = 0;
+            char currentOperand = default;
+            void ApplyAction(long value)
+            {
+                if (currentOperand == default(char))
+                    currentTotal = value;
+                else if (currentOperand == '+')
+                    currentTotal += value;
+                else
+                    currentTotal *= value;
+            }
+            for (var n = 0; n < equation.Length; n++)
+            {
+                var c = equation[n];
+                if (char.IsDigit(c))
+                {
+                    ApplyAction(c - '0');
+                }
+                else if (c == '(')
+                {
+                    // find matching end bracket
+                    var count = 1;
+                    for (var k = n+1; n < equation.Length; k++)
+                    {
+                        if (equation[k] == ')')
+                        {
+                            count--;
+                            if (count == 0)
+                            {
+                                var subEquation = equation.Substring(n + 1, k - n - 1);
+                                ApplyAction(SolveEquation(subEquation));
+                                n = k + 1; // jump past this equation
+                                break;
+                            }
+                        }
+                        else if (equation[k] == '(')
+                        {
+                            count++;
+                        }
+                    }
+                    if (count != 0) throw new InvalidOperationException("failed to parse subequation");
+                }
+                else if (c == '+' || c == '*')
+                {
+                    currentOperand = c;
+                }
+
+            }
+            return currentTotal;
         }
     }
 }
--- UnitTests/Day18Tests.cs
+++ UnitTests/Day18Tests.cs
@@ -5,12 +5,16 @@ namespace UnitTests
 {
     public class Day18Tests
     {
-        [Test]
-        public void SolveWithTestInput()
+        [TestCase("1 + 2 * 3 + 4 * 5 + 6", 71)]
+        [TestCase("1 + (2 * 3) + (4 * (5 + 6))", 51)]
+        [TestCase("2 * 3 + (4 * 5)", 26)]
+        [TestCase("5 + (8 * 3 + 9 + 3 * 4 * 3)", 437)]
+        [TestCase("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 12240)]
+        [TestCase("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632)]
+        public void SolveEquation(string equation, long expected)
         {
-            var testInput = @"".Split("\r\n");
-            var answer = new Day18().Solve(testInput);
-            Assert.AreEqual(("a", "b"), answer);
+            var calculated = Day18.SolveEquation(equation);
+            Assert.AreEqual(expected, calculated);
         }
     }
 }
