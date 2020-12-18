93d4faf608c45ef8517fce2f80acf2a43c860228
Mark Heath <mark.heath@gmail.com>
2020-12-18 08:20:11 +0000

    day 18 part 2

--- AdventOfCode2020/Day18.cs
+++ AdventOfCode2020/Day18.cs
@@ -1,16 +1,122 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode2020
 {
     public class Day18 : ISolver
     {
-        public (string, string) ExpectedResult => ("67800526776934", "");
+        public (string, string) ExpectedResult => ("67800526776934", "340789638435483");
 
         public (string, string) Solve(string[] input)
         {
             var part1 = input.Sum(SolveEquation);
-            return (part1.ToString(), "");
+            var part2 = input.Sum(SolveEquation2);
+
+            return (part1.ToString(), part2.ToString());
+        }
+
+        abstract class Token { }
+        abstract class Operator : Token { }
+        class Multiply : Token { }
+        class Add : Token { }
+        class Number : Token
+        {
+            public Number(long value)
+            {
+                Value = value;
+            }
+            public long Value { get; }
+        }
+
+        class Equation : Token
+        {
+            public Equation()
+            {
+                Tokens = new List<Token>();
+            }
+            public List<Token> Tokens { get; }
+
+            public long Solve()
+            {
+                // 1. solve all subequations
+                for (int n = 0; n < Tokens.Count; n++)
+                {
+                    if (Tokens[n] is Equation eq)
+                    {
+                        Tokens[n] = new Number(eq.Solve());
+                    }
+                }
+
+                // perform all additions
+                var k = 0;
+                while(k < Tokens.Count)
+                {
+                    if (Tokens[k] is Add)
+                    {
+                        var newValue = ((Number)Tokens[k - 1]).Value + ((Number)Tokens[k + 1]).Value;
+                        Tokens[k - 1] = new Number(newValue);
+                        Tokens.RemoveRange(k, 2);
+                    }
+                    else
+                    {
+                        k++;
+                    }
+                }
+
+                // we only have multiplications left
+                return Tokens.OfType<Number>().Select(n => n.Value)
+                    .Aggregate((a, b) => a * b);
+            }
+        }
+
+        static Equation Tokenize(string equation)
+        {
+            var eq = new Equation();
+            for(int n = 0; n < equation.Length; n++)
+            {
+                var c = equation[n];
+                if (char.IsDigit(c)) eq.Tokens.Add(new Number(c - '0'));
+                else if (c == '*') eq.Tokens.Add(new Multiply());
+                else if (c == '+') eq.Tokens.Add(new Add());
+                else if (c == '(')
+                {
+                    var subEq = ExtractBetweenMatchingBrackets(equation, n);
+                    eq.Tokens.Add(Tokenize(subEq));
+                    n += subEq.Length;
+                }
+            }
+            return eq;
+        }
+
+        static string ExtractBetweenMatchingBrackets(string s, int startPos)
+        {
+            // find matching end bracket
+            var count = 1;
+            for (var k = startPos + 1; k < s.Length; k++)
+            {
+                if (s[k] == ')')
+                {
+                    count--;
+                    if (count == 0)
+                    {
+                        var subEquation = s.Substring(startPos + 1, k - startPos - 1);
+                        return subEquation;
+                    }
+                }
+                else if (s[k] == '(')
+                {
+                    count++;
+                }
+            }
+            throw new InvalidOperationException("failed to parse subequation");
+        }
+
+
+        public static long SolveEquation2(string equation)
+        {
+            var eq = Tokenize(equation);
+            return eq.Solve();
         }
 
         public static long SolveEquation(string equation)
@@ -35,27 +141,9 @@ namespace AdventOfCode2020
                 }
                 else if (c == '(')
                 {
-                    // find matching end bracket
-                    var count = 1;
-                    for (var k = n+1; n < equation.Length; k++)
-                    {
-                        if (equation[k] == ')')
-                        {
-                            count--;
-                            if (count == 0)
-                            {
-                                var subEquation = equation.Substring(n + 1, k - n - 1);
-                                ApplyAction(SolveEquation(subEquation));
-                                n = k + 1; // jump past this equation
-                                break;
-                            }
-                        }
-                        else if (equation[k] == '(')
-                        {
-                            count++;
-                        }
-                    }
-                    if (count != 0) throw new InvalidOperationException("failed to parse subequation");
+                    var subEquation = ExtractBetweenMatchingBrackets(equation, n);
+                    ApplyAction(SolveEquation(subEquation));
+                    n += subEquation.Length;
                 }
                 else if (c == '+' || c == '*')
                 {
--- UnitTests/Day18Tests.cs
+++ UnitTests/Day18Tests.cs
@@ -16,5 +16,17 @@ namespace UnitTests
             var calculated = Day18.SolveEquation(equation);
             Assert.AreEqual(expected, calculated);
         }
+
+        [TestCase("1 + 2 * 3 + 4 * 5 + 6", 231)]
+        [TestCase("1 + (2 * 3) + (4 * (5 + 6))", 51)]
+        [TestCase("2 * 3 + (4 * 5)", 46)]
+        [TestCase("5 + (8 * 3 + 9 + 3 * 4 * 3)", 1445)]
+        [TestCase("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 669060)]
+        [TestCase("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 23340)]
+        public void SolveEquationPart2(string equation, long expected)
+        {
+            var calculated = Day18.SolveEquation2(equation);
+            Assert.AreEqual(expected, calculated);
+        }
     }
 }
