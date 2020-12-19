b5a6cae926b5bea703e1a27d711ce703292fe510
Mark Heath <mark.heath@gmail.com>
2020-12-19 08:13:40 +0000

    day 19 part 1

--- AdventOfCode2020/Day19.cs
+++ AdventOfCode2020/Day19.cs
@@ -1,14 +1,106 @@
-﻿using System;
+﻿using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode2020
 {
     public class Day19 : ISolver
     {
-        public (string, string) ExpectedResult => ("", "");
+        public (string, string) ExpectedResult => ("156", "");
 
         public (string, string) Solve(string[] input)
         {
-            throw new NotImplementedException();
+            var part1 = CountValidMessages(input);
+            return (part1.ToString(), "");
         }
+
+        public static int CountValidMessages(string[] input)
+        {
+            var sections = input.Split("").ToArray();
+            var ruleset = new RuleSet(sections[0]);
+            return sections[1].Count(m => ruleset.IsFullMatch(m));
+        }
+
+        public class RuleSet
+        {
+            public Dictionary<int, Rule> Rules { get; }
+
+            public RuleSet(IEnumerable<string> rules)
+            {
+                Rules = rules.Select(r => new Rule(r)).ToDictionary(r => r.Id, r => r);
+            }
+
+            public bool IsFullMatch(string message)
+            {
+                var (match, pos) = Check(message);
+                return match && pos == message.Length;
+            }
+
+            public (bool,int) Check(string message, int startingRule = 0, int startPos = 0)
+            {
+                var rule = Rules[startingRule];
+                if (rule.Match != null)
+                {
+                    if (message.Substring(startPos).StartsWith(rule.Match))
+                        return (true, startPos + rule.Match.Length);
+                    return (false,startPos);
+                }
+                else
+                {
+                    bool isMatched = false;
+                    foreach(var sequence in rule.Sequences)
+                    {
+                        var sequenceMatch = false;
+                        var currentPos = startPos;
+                        foreach(var ruleId in sequence)
+                        {
+                            (isMatched, currentPos) = Check(message, ruleId, currentPos);
+                            if (!isMatched) break;
+                        }
+                        isMatched |= sequenceMatch;
+                        if (isMatched) return (true, currentPos);
+                    }
+                    return (false, startPos);
+                }
+            }
+
+        }
+
+        public class Rule
+        {
+            public int Id { get; }
+            public List<List<int>> Sequences { get; }
+            public string Match { get; }
+
+            public Rule(string rule)
+            {
+                var bits = rule.Split(' ');
+                Id = int.Parse(bits[0].Substring(0, bits[0].Length - 1));
+                Sequences = new List<List<int>>();
+                List<int> currentSequence = null;
+                for (var n = 1; n < bits.Length; n++)
+                {
+                    if (bits[n].StartsWith('"'))
+                    {
+                        Match = bits[n].Substring(1, 1); // always 1 char
+                    }
+                    else if (bits[n] == "|")
+                    {
+                        currentSequence = null;
+                    }
+                    else 
+                    {
+                        if (currentSequence == null)
+                        {
+                            currentSequence = new List<int>();
+                            Sequences.Add(currentSequence);
+                        }
+                        currentSequence.Add(int.Parse(bits[n]));
+                    }
+                }
+            }
+        }
+
     }
 }
--- UnitTests/Day19Tests.cs
+++ UnitTests/Day19Tests.cs
@@ -1,16 +1,74 @@
 ﻿using AdventOfCode2020;
 using NUnit.Framework;
+using System.Linq;
 
 namespace UnitTests
 {
     public class Day19Tests
     {
         [Test]
-        public void SolveWithTestInput()
+        public void ParseRules()
         {
-            var testInput = @"".Split("\r\n");
-            var answer = new Day19().Solve(testInput);
-            Assert.AreEqual(("a", "b"), answer);
+            var testInput = @"0: 1 2
+1: ""a""
+2: 1 3 | 3 1
+3: ""b""".Split("\r\n");
+            var rules = testInput.Select(r => new Day19.Rule(r)).ToList();
+            Assert.AreEqual(0, rules[0].Id);
+            Assert.AreEqual(new[] { 1, 2 }, rules[0].Sequences[0]);
+            Assert.AreEqual(1, rules[0].Sequences.Count);
+            Assert.IsNull(rules[0].Match);
+
+            Assert.AreEqual(1, rules[1].Id);
+            Assert.AreEqual(0, rules[1].Sequences.Count);
+            Assert.AreEqual("a", rules[1].Match);
+
+            Assert.AreEqual(2, rules[2].Id);
+            Assert.AreEqual(new[] { 1, 3 }, rules[2].Sequences[0]);
+            Assert.AreEqual(new[] { 3, 1 }, rules[2].Sequences[1]);
+            Assert.AreEqual(2, rules[2].Sequences.Count);
+            Assert.IsNull(rules[2].Match);
+
+            Assert.AreEqual(3, rules[3].Id);
+            Assert.AreEqual(0, rules[3].Sequences.Count);
+            Assert.AreEqual("b", rules[3].Match);
+
         }
+
+        [TestCase("ababbb",true)]
+        [TestCase("abbbab", true)]
+        [TestCase("bababa", false)]
+        [TestCase("aaabbb", false)]
+        [TestCase("aaaabbb", false)] // might be true, but not a full match
+        public void ApplyRules(string message, bool valid)
+        {
+            var testInput = @"0: 4 1 5
+1: 2 3 | 3 2
+2: 4 4 | 5 5
+3: 4 5 | 5 4
+4: ""a""
+5: ""b""".Split("\r\n");
+            var ruleset = new Day19.RuleSet(testInput);
+            Assert.AreEqual(valid, ruleset.IsFullMatch(message));
+        }
+
+        [Test]
+        public void CountValidMessages()
+        {
+            var testInput = @"0: 4 1 5
+1: 2 3 | 3 2
+2: 4 4 | 5 5
+3: 4 5 | 5 4
+4: ""a""
+5: ""b""
+
+ababbb
+bababa
+abbbab
+aaabbb
+aaaabbb".Split("\r\n");
+            Assert.AreEqual(2, Day19.CountValidMessages(testInput));
+        }
+
     }
 }
