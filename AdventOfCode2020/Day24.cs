f7c749a8a75c819c5c3f04105b6f35f5006bd96d
Mark Heath <mark.heath@gmail.com>
2020-12-24 08:43:46 +0000

    day 24 part 1

--- AdventOfCode2020/Day24.cs
+++ AdventOfCode2020/Day24.cs
@@ -1,15 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode2020
 {
     public class Day24 : ISolver
     {
-        public (string, string) ExpectedResult => ("", "");
+        public (string, string) ExpectedResult => ("244", "");
+
+        // axes are n/s, ne/sw, nw,se
+        //          e/w, ne/sw, nw,se
+        private static readonly Dictionary<string, (int, int, int)> lookup = new Dictionary<string, (int, int, int)> {
+            { "nw",  (0, 1, -1) },
+            { "se",  (0, -1, 1) },
+            { "w", (-1, 1, 0) },
+            { "e", (1, -1, 0) },
+            { "ne", (1, 0, -1) },
+            { "sw", (-1, 0, 1) }
+        };
+
+        public static (int,int,int) FollowPath(string path)
+        {
+            var pos = (0, 0, 0);
+            foreach(var (x,y,z) in ParseDir(path).Select(d => lookup[d]))
+            {
+                pos = (pos.Item1 + x, pos.Item2 + y, pos.Item3 + z);
+            }
+            return pos;
+        }
+
+        // e, se, sw, w, nw, and ne
+        public static IEnumerable<string> ParseDir(string input)
+        {
+            for(var n =0; n < input.Length; n++)
+            {
+                if (input[n] == 'n' || input[n] == 's')
+                {
+                    yield return input[n..(n + 2)];
+                    n++;
+                }
+                else
+                {
+                    yield return input[n..(n+1)];
+                }
+            }
+        }
 
         public (string, string) Solve(string[] input)
         {
-            throw new NotImplementedException();
+            HashSet<(int, int, int)> blackTiles = new HashSet<(int, int, int)>();
+            foreach(var line in input)
+            {
+                var pos = FollowPath(line);
+                if (blackTiles.Contains(pos)) 
+                    blackTiles.Remove(pos); 
+                else 
+                    blackTiles.Add(pos);
+            }
+            var part1 = blackTiles.Count;
+            return (part1.ToString(), "");
         }
     }
 }
--- UnitTests/Day24Tests.cs
+++ UnitTests/Day24Tests.cs
@@ -1,16 +1,50 @@
 ﻿using AdventOfCode2020;
 using NUnit.Framework;
+using System.Linq;
 
 namespace UnitTests
 {
     public class Day24Tests
     {
         [Test]
+        public void CanParseDirections()
+        {
+            var d = Day24.ParseDir("neeenesenwnww").ToArray();
+            Assert.AreEqual(new[] { "ne", "e", "e", "ne", "se", "nw", "nw", "w" }, d);
+        }
+
+        [Test]
+        public void FollowPath()
+        {
+            var d = Day24.FollowPath("nwwswee");
+            Assert.AreEqual((0, 0, 0), d);
+        }
+
+        [Test]
         public void SolveWithTestInput()
         {
-            var testInput = @"".Split("\r\n");
+            var testInput = @"sesenwnenenewseeswwswswwnenewsewsw
+neeenesenwnwwswnenewnwwsewnenwseswesw
+seswneswswsenwwnwse
+nwnwneseeswswnenewneswwnewseswneseene
+swweswneswnenwsewnwneneseenw
+eesenwseswswnenwswnwnwsewwnwsene
+sewnenenenesenwsewnenwwwse
+wenwwweseeeweswwwnwwe
+wsweesenenewnwwnwsenewsenwwsesesenwne
+neeswseenwwswnwswswnw
+nenwswwsewswnenenewsenwsenwnesesenew
+enewnwewneswsewnwswenweswnenwsenwsw
+sweneswneswneneenwnewenewwneswswnese
+swwesenesewenwneswnwwneseswwne
+enesenwswwswneneswsenwnewswseenwsese
+wnwnesenesenenwwnenwsewesewsesesew
+nenewswnwewswnenesenwnesewesw
+eneswnwswnwsenenwnwnwwseeswneewsenese
+neswnwewnwnwseenwseesewsenwsweewe
+wseweeenwnesenwwwswnew".Split("\r\n");
             var answer = new Day24().Solve(testInput);
-            Assert.AreEqual(("a", "b"), answer);
+            Assert.AreEqual(("10", ""), answer);
         }
     }
 }
