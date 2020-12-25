32487bd13396920b0289261335bd5b2e073e3ff0
Mark Heath <mark.heath@gmail.com>
2020-12-25 07:49:23 +0000

    day 25 part 1

--- AdventOfCode2020/Day25.cs
+++ AdventOfCode2020/Day25.cs
@@ -5,11 +5,70 @@ namespace AdventOfCode2020
 {
     public class Day25 : ISolver
     {
-        public (string, string) ExpectedResult => ("", "");
+        public (string, string) ExpectedResult => ("6408263", "");
+
+        public long Transform(long subject, int loopSize)
+        {
+            var value = 1L;
+            for (var n = 0; n < loopSize; n++)
+            {
+                // Set the value to itself multiplied by the subject number.
+                value *= subject;
+                // Set the value to the remainder after dividing the value by 20201227.
+                value %= 20201227;
+            }
+            return value;
+        }
+
+        public (int,long) Search(long subject, long pk1, long pk2)
+        {
+            var value = 1L;
+            for (var loopSize = 1; true; loopSize++)
+            {
+                // Set the value to itself multiplied by the subject number.
+                value *= subject;
+                // Set the value to the remainder after dividing the value by 20201227.
+                value %= 20201227;
+                if (value == pk1) return (loopSize, pk1);
+                if (value == pk2) return (loopSize, pk2);
+            }
+            throw new InvalidOperationException();
+        }
+
+        public int FindSecretLoopSize(long subjectNumber, long expectedPublicKey)
+        {
+            for (var n = 1; true; n++)
+            {
+                if (Transform(subjectNumber, n) == expectedPublicKey)
+                    return n;
+
+            }
+            throw new InvalidOperationException();
+        }
+
+        public void Handshake(long subjectNumber, int cardSecretLoopSize, int doorSecretLoopSize)
+        {
+            var cardPublicKey = Transform(subjectNumber, cardSecretLoopSize);
+            var doorPublicKey = Transform(subjectNumber, doorSecretLoopSize);
+
+            var encryptionKey = Transform(doorPublicKey, cardSecretLoopSize);
+            var shouldBeSameEncryptionKey = Transform(cardPublicKey, doorSecretLoopSize);
+
+
+
+        }
 
         public (string, string) Solve(string[] input)
         {
-            throw new NotImplementedException();
+            var cardPublicKey = long.Parse(input[0]);
+            var doorPublicKey = long.Parse(input[1]);
+            var (loopSize, pk) = Search(7, doorPublicKey, cardPublicKey);
+            long encryptionKey;
+            if (pk == doorPublicKey)
+                encryptionKey = Transform(cardPublicKey, loopSize);
+            else
+                encryptionKey = Transform(doorPublicKey, loopSize);
+            return (encryptionKey.ToString(), "");
         }
     }
 }
--- UnitTests/Day25Tests.cs
+++ UnitTests/Day25Tests.cs
@@ -6,11 +6,26 @@ namespace UnitTests
     public class Day25Tests
     {
         [Test]
-        public void SolveWithTestInput()
+        public void FindLoopSize1()
         {
-            var testInput = @"".Split("\r\n");
+            var answer = new Day25().FindSecretLoopSize(7, 5764801);
+            Assert.AreEqual(8, answer);
+        }
+
+        [Test]
+        public void FindLoopSize2()
+        {
+            var answer = new Day25().FindSecretLoopSize(7, 17807724);
+            Assert.AreEqual(11, answer);
+        }
+
+        [Test]
+        public void RunWithTestInput()
+        {
+            var testInput = @"5764801
+17807724".Split("\r\n");
             var answer = new Day25().Solve(testInput);
-            Assert.AreEqual(("a", "b"), answer);
+            Assert.AreEqual("14897079", answer.Item1);
         }
     }
 }
